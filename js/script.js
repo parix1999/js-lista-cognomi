7a325914ad2fbe1fd04ef607771a53ec0bd45200
parix1999 <parisottoedoardo@gmail.com>
2021-06-03 17:02:20 +0200

    cognome- 7.0

--- js/script.js
+++ js/script.js
@@ -4,21 +4,30 @@
 // 4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
 
 
-var elementDiv = document.getElementsByClassName('risultato');
-var outPut = elementDiv[0];
+// var elementDiv = document.getElementsByClassName('risultato');
+// var outPut = elementDiv[0];
+
+// var listaNomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli','Ciano'];
+// var nome = prompt('Inserisci il tuo cognome ');
+
+// listaNomi.push(nome);
+// listaNomi.sort();
+// outPut.innerHTML= listaNomi ;
+
+ 
+
+// // posizione umana
+// for (var x = 1; x < listaNomi.length; x++) ; {
+//     console.log(listaNomi[0]);
+//     outPut.innerHTML += '<br>' + 'posizione del nome è ' + listaNomi.indexOf(nome);
+// }
+
+
+
 
 var listaNomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli','Ciano'];
 
+var nomeUtente = prompt('Inserisci il tuo nome');
 
-for (var n = 0; n < listaNomi.length; n++) ; {
-    var nome = listaNomi.push(prompt('Inserisci il tuo cognome '));
-    listaNomi.sort();
-    var text= listaNomi ;
-}
-outPut.innerHTML = text ; 
-
-// posizione umana
-for (var x = 1; x < listaNomi.length; x++) ; {
-    console.log(listaNomi[0]);
-    outPut.innerHTML += '<br>' + 'posizione del nome è ' + listaNomi.indexOf[nome];
-}
+console.log(listaNomi);
+console.log(nomeUtente);
