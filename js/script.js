32df8be563e2770ad6996e1bc9b4089c87415d72
parix1999 <parisottoedoardo@gmail.com>
2021-06-03 15:04:18 +0200

    cognomi-1.0

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// 1. chiedi all’utente il cognome, con un promot
+// 2. inseriscilo in un array dove sono presenti già altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente, sicuramente con una funzione
+// 4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
+
+var nome = prompt('Inserisci il tuo nome nella lista');
+
+var listaNomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli','Ciano'];
+
+for (var n = 0; n < listaNomi.length; n++) ; {
+    listaNomi.push(nome);
+}
